6ef61f3ba6392f014658861243d887914a7703e1
nguyenminhtu <tuunguyen2795@gmail.com>
2017-07-21 23:28:53 +0700

    Login (chap8)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if user.save
+      log_in user
       flash[:success] = t "users.new.success"
       redirect_to user
     else
@@ -20,8 +21,9 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
-    params.require(:user).permit :name, :email,
-      :password, :password_confirmation
+    params.require(:user).permit :name,
+      :email, :password, :password_confirmation
   end
 end
