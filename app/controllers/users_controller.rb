c53d07f8b246e09f4dc73f7893fef45492acae6a
nguyenminhtu <tuunguyen2795@gmail.com>
2017-07-21 23:28:53 +0700

    Sign up (chap 7)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+  attr_reader :user
+
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
+  def create
+    @user = User.new user_params
+    if user.save
+      flash[:success] = t "users.new.success"
+      redirect_to user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email,
+      :password, :password_confirmation
   end
 end
